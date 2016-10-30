0cd85464060df9cfd51f0ce705332a6c2655f7bd
Hiroshi Saito <saidie@saidie.info>
2016-10-31 00:28:55 +0900

    Provide wvltr.rangefreq

--- src/module.c
+++ src/module.c
@@ -302,6 +302,10 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
             WaveletTreeQuantile_RedisCommand, "write deny-oom", 1, 1, 1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
+    if (RedisModule_CreateCommand(ctx, "wvltr.rangefreq",
+            WaveletTreeRangeFreq_RedisCommand, "write deny-oom", 1, 1, 1) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+
     return REDISMODULE_OK;
 }
 
