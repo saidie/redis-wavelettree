5922f1b98a41883d67a8ba00a6fe597d2afe4d21
Hiroshi Saito <hiroshi.saito@dena.com>
2016-10-30 15:35:29 +0900

    Add WaveletTreeType_Load

--- src/module.c
+++ src/module.c
@@ -1,6 +1,21 @@
 #include "redismodule.h"
 #include "wavelet_tree.h"
 
+void *WaveletTreeType_Load(RedisModuleIO *rdb, int encver) {
+    if (encver != 0) return NULL;
+
+    uint32_t i;
+    uint32_t len = RedisModule_LoadUnsigned(rdb);
+    int32_t *buffer = RedisModule_Calloc(len, sizeof(uint32_t));
+
+    for(i = 0; i < len; ++i)
+        buffer[i] = RedisModule_LoadSigned(rdb);
+
+    wt_tree *tree = wt_new();
+    wt_build(tree, buffer, len);
+    return tree;
+}
+
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     if (RedisModule_Init(ctx, "wvltr", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
