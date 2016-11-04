997091f924d33eb75e1cdb202f7ec314a94797de
Hiroshi Saito <saidie@saidie.info>
2016-11-05 00:50:15 +0900

    Provide wvltr.nextvalue

--- src/module.c
+++ src/module.c
@@ -447,6 +447,10 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
             WaveletTreePrevValue_RedisCommand, "write deny-oom", 1, 1, 1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
+    if (RedisModule_CreateCommand(ctx, "wvltr.nextvalue",
+            WaveletTreeNextValue_RedisCommand, "write deny-oom", 1, 1, 1) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+
     return REDISMODULE_OK;
 }
 
